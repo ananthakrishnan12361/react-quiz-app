9ac9b6c5eec8183e66656af95bf1ae0d96ab0495
ananthakrishnan12361 <ananthakrishnan12361@gmail.com>
2023-12-28 11:16:04 +0530

    Explanation added to QuizTest

--- src/components/QuizTest.js
+++ src/components/QuizTest.js
@@ -3,8 +3,10 @@ import QuizCard from './shared/QuizCard'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 export default function QuizTest({quizData,setTotalScore,setPosition,setQuizData}) {
+  // State variables using the useState hook
   const [selectedAnswers, setSelectedAnswers] = useState({});
   const [show, setShow] = useState(false);
+  // Function to handle option selection in a quiz questio
   const handleOptionSelect = (questionId, selectedOption) => {
     setSelectedAnswers({
       ...selectedAnswers,
@@ -12,26 +14,54 @@ export default function QuizTest({quizData,setTotalScore,setPosition,setQuizData
     });
   };
 
+  // Function to calculate the total score based on selected answers
   const handleFinish =async () => {
     const score =await calculateScore();
     setTotalScore(score);
     setPosition('result')
   };
 
+   // Function to handle finishing the quiz
   const calculateScore = () => {
     return quizData.reduce((score, question) => {
       const isCorrect = selectedAnswers[question.question] === question.correct_answer;
       return isCorrect ? score + 1 : score;
     }, 0);
   };
+
+    // Function to handle canceling the quiz
 const handleCancel=()=>{
   setTotalScore(0)
   setSelectedAnswers({})
   setQuizData([])
   setPosition('register')
 }
+ // Functions to handle Modal visibility
 const handleClose = () => setShow(false);
 const handleShow = () => setShow(true);
+
+Explanation:
+
+// State Variables:
+
+// selectedAnswers: Keeps track of the user's selected answers for each quiz question.
+// show: Manages the visibility of the cancel confirmation modal.
+// Functions:
+
+// handleOptionSelect: Updates the selectedAnswers state when a user selects an option for a quiz question.
+// calculateScore: Calculates the total score based on the selected answers.
+// handleFinish: Handles the process of finishing the quiz, calculating the score, and updating the position to 'result'.
+// handleCancel: Handles the process of canceling the quiz, resetting state variables, and updating the position to 'register'.
+// handleClose and handleShow: Functions to manage the visibility of the cancel confirmation modal.
+// Rendering:
+
+// Conditionally renders quiz questions and options if quizData is available.
+// Displays an error message and a button to go back to the start if there is an issue with quiz data.
+// Provides buttons for finishing the quiz and canceling the quiz.
+// Modal:
+
+// Utilizes the Bootstrap Modal component to confirm the cancellation of the quiz.
+
   return (
     <div  className='' style={{ overflowY: "auto", height: "75vh", scrollbarWidth: 'none !important',overflowX: 'hidden', }} >
       {
@@ -62,6 +92,7 @@ const handleShow = () => setShow(true);
         </div>
         </>
       }
+      {/* Modal for confirming quiz cancellation */}
         <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>Cancel Quiz</Modal.Title>
